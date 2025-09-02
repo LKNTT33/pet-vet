a5fdf45ba756c1262413cbe3b4ff165367bbe822
ricardo moitas <ricardo.moitas04@gmail.com>
2025-09-02 16:54:24 +0100

    seed updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: {owner: 0, vet: 1}
+
+
+  has_many :pets, dependent: :destroy
+  has_many :availabilities, dependent: :destroy
+  has_many :appointments, through: :availabilities, as: :vet_appointments
+  has_many :appointments, through: :pets, as: :owner_appointments
 end
