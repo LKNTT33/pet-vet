6fae76fa6a23ffe421ea6f3a1b16e5d6139b9261
JoÃao Sobral <joaosobral1234@gmail.com>
2025-09-03 10:47:38 +0100

    routing paths login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'vets/index'
   get 'vets/show'
   get 'users/show'
-  devise_for :users
+  
+  devise_for :users, controllers: {
+  registrations: "users/registrations",
+  sessions: "users/sessions"
+}
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
